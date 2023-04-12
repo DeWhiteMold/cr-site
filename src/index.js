60e81d98bae575336e5044d77f0955eb5bf073cc
Nikolai Smirnov <dewhitemold@yandex.ru>
2023-04-12 16:31:01 +0200

    meta changed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
+import { hydrate, render } from "react-dom";
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
